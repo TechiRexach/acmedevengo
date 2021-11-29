f07aad23e1f4cbf39bfed5872cd98e5dda57540e
Techi Rexach <techi90@gmail.com>
2021-11-29 20:55:25 +0100

    first merge between csv files and create the output csv file

--- employees_payroll.rb
+++ employees_payroll.rb
@@ -3,13 +3,34 @@ require 'csv'
 
 hcm = 'hcm.csv'
 payroll = 'payroll.csv'
+result = 'result.csv'
 
-csv_hcm = CSV.read(hcm, headers: true)
+# csv_hcm = CSV.read(hcm, headers: true)
 
-csv_pay = CSV.read(payroll, headers: true)
+# csv_pay = CSV.read(payroll, headers: true)
 
-puts csv_hcm
+target1 = 'ID'
+target2 = 'worker_id'
 
-puts 'holi'
+new_target = target1 + '/' + target2
 
-puts csv_pay
+csv1 = CSV.read(hcm, headers: true, header_converters: lambda {|header| header == target1 ? new_target : header})
+
+csv2 = CSV.read(payroll, headers: true)
+
+headers1 = csv1.headers
+
+headers2 = csv2.headers - [target2]
+
+# csv1[i][new_target] == csv2[i][target2]
+
+CSV.open(result, 'w') do |csv|
+    csv << headers1 + headers2
+    [csv1.size, csv2.size].min.times do |i|
+        csv << (headers1.map {|h| csv1[i][h]} + 
+        headers2.map {|h| csv2[i][h] }) if 
+            csv1[i][new_target] == csv2[i][target2]
+    end 
+end
+
+puts File.read(result)
