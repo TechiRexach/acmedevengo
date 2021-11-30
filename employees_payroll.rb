c5b3df1109e12483b3ec8d71a1a2fb926b005bfc
Techi Rexach <techi90@gmail.com>
2021-11-30 20:22:11 +0100

    differents csv for ok and errors

--- employees_payroll.rb
+++ employees_payroll.rb
@@ -1,36 +1,74 @@
 require 'rails/all'
 require 'csv'
+require "awesome_print"
 
-hcm = 'hcm.csv'
-payroll = 'payroll.csv'
-result = 'result.csv'
+#leemos todos los datos del csv
+csv_hcm = CSV.read("hcm.csv", headers: true)
+#ap csv_hcm.map{|l| l.to_h}
 
-# csv_hcm = CSV.read(hcm, headers: true)
+#creamos el hash nuevo
+employees_data = Hash.new
 
-# csv_pay = CSV.read(payroll, headers: true)
-
-target1 = 'ID'
-target2 = 'worker_id'
-
-new_target = target1 + '/' + target2
-
-csv1 = CSV.read(hcm, headers: true, header_converters: lambda {|header| header == target1 ? new_target : header})
-
-csv2 = CSV.read(payroll, headers: true)
-
-headers1 = csv1.headers
-
-headers2 = csv2.headers - [target2]
-
-# csv1[i][new_target] == csv2[i][target2]
-
-CSV.open(result, 'w') do |csv|
-    csv << headers1 + headers2
-    [csv1.size, csv2.size].min.times do |i|
-        csv << (headers1.map {|h| csv1[i][h]} + 
-        headers2.map {|h| csv2[i][h] }) if 
-            csv1[i][new_target] == csv2[i][target2]
-    end 
+#de cada row seleccionamos el ID como identificador y rellenamos el hash nuevo
+csv_hcm.each do |employee|
+    employees_data[ employee["ID"] ] = employee.to_h
 end
 
-puts File.read(result)
+#ap employees_data
+
+csv_payroll = CSV.read("payroll.csv", headers: true)
+
+#preparamos las dos posibles uniones, las que dan error y las que estan ok
+merge = {errors: [], ok: []}
+
+def valid?(e)
+    e['ID'] != nil
+  #ap e['TELEPHONE'] != nil
+#   xxxx
+    #e[:errors] = "Falta teléfono"
+    e
+end
+
+#recorremos fichero nominas
+csv_payroll.each do |payroll|
+    #traemos el id del empleado desde el cvs de empleados (OJO QUE SIN DATOS DE NOMINA NO LO TRAE)
+    employee = employees_data[payroll['worker_id']]
+    #ap employee
+    #comprobamos que ese empleado existe en el fichero
+    if employee
+        employee_payroll = payroll.to_h.merge(employee)
+        # ap employee_payroll
+        if employee_payroll = valid?(employee_payroll)
+            merge[:ok] << employee_payroll
+            #ap merge[:ok]
+        else
+            merge[:errors] << employee_payroll
+            ap employee_payroll
+        end
+    else
+        
+        employee_payroll = payroll.to_h
+        employee_payroll[:errors] = 'Nómina sin datos de empleado'
+        merge[:errors] << employee_payroll
+        #ap merge[:errors]
+    end
+end
+
+CSV.open('result.csv', 'w') do |csv|
+
+    csv << %w(id dni name surname telephone email user_principal_name area iban start_date end_date company_id company_name net_salary extrapayroll_1 extrapayroll_2 extrapayroll_3 )
+
+    merge[:ok].each do |employee| 
+        ap employee
+        csv << employee.values_at('ID', 'DNI', 'NAME', 'SURNAME', 'TELEPHONE', 'EMAIL', 'USER_PRINCIPAL_NAME', 'AREA', 'IBAN', 'START_DATE', 'END_DATE', 'COMPANY_ID', 'COMPANY_NAME', 'net_salary', 'extrapayroll_1', 'extrapayroll_2', 'extrapayroll_3')
+    end
+end
+
+CSV.open('errors.csv', 'w') do |csv|
+
+    csv << %w(id dni name surname telephone email user_principal_name area iban start_date end_date company_id company_name net_salary extrapayroll_1 extrapayroll_2 extrapayroll_3 )
+
+    merge[:errors].each do |employee| 
+        csv << employee.values_at('ID', 'DNI', 'NAME', 'SURNAME', 'TELEPHONE', 'EMAIL', 'USER_PRINCIPAL_NAME', 'AREA', 'IBAN', 'START_DATE', 'END_DATE', 'COMPANY_ID', 'COMPANY_NAME', 'net_salary', 'extrapayroll_1', 'extrapayroll_2', 'extrapayroll_3')
+    end
+end
