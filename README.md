ac5b95437da67bb4e7ae5970dde5586e51f20428
Techi Rexach <techi90@gmail.com>
2021-12-03 12:06:09 +0100

    deleted code comments and updated readme

--- README.md
+++ README.md
@@ -1,7 +1,38 @@
 # acmedevengo
 
-
 ## Explanation:
 
+We recieve daily an employees data file of the ACME Corp, wich is name we are going to assume that has been accorded as ```"hcm_YYYYMMDD.csv"```.
 
+Equally, but monthly, we recieve a payroll data file, with the name accorded as ```"payroll_YYYYMMDD.csv"``` as well as the employee file.
+
+(The files we are going to work with in this code are not named like this to make it easier to test them).
+
+First we should keep a copy of the original files (one daily, one monthly), just in case the company notify any errors. This provides a way to check if it is ours fault or if the company sent a bad data.
+
+As we recieve the payroll file just once per month, we will do the merge about this file, adding the updates we recieve daily in the employees file. Also, if it is needed by the company, like at the middle of the month, we could make an update of the payroll data without any inconvenience.
+
+Both files are related by the employee ID, so this will be our key in all the development.
+
+We read each csv file and store the data in two new hashes using the employees ID as the keys. We now have the ```employees_data``` hash and the ```payroll_data``` hash.
+
+Now, we will do a merge between the employees hash and the payroll hash, adding the employees ID that do not exist in the payroll hash, such as employees without payroll data. This means that now our ```payroll_data``` has all the employees ID without any of them being duplicated.
+
+We set two possible outputs ```(:ok and :errors)``` to control if any of the data contains any error, one will contain the data that when doing the merge does not give error and the other will contain the data that must be indicated to the company to be checked.
+
+We will also define a ``(validate)`` method that allows us to add a new column for errors in the final output. These types of errors will not condition the merge between both data, but may be important for the HR team to be aware of.
+
+Then we go through our payroll hash where we already have all the employees and declare a variable for each one. We check that each payroll has the employee's data and verify with our ```validate``` method that it has all the important data. If this is not the case, the corresponding information will be added in the new column we created previously.
+
+After that, we save the resulting data in the ```:ok``` output, as we are considering that missing employee data does not break the process.
+
+In case there is a payroll with an employee ID that does not exist in the employee file sent by the company, we will add that data to the output of ```:errors```, in order to inform the company.
+
+Finally, we are going to export the outputs with the data merged in two files (```"result_YYYYMMDD.csv"``` and ```"errors_YYYYMMDD.csv"```) that will be automatically named the same but with the daily date so that they are not overwritten and we can maintain a good consistency of the data during the whole month. This will allow us to be able to send it at any time to the company if we are requested to do so at any time.
+
+## Usage:
+
+This code has been written in Ruby language, so make sure to have the Rails framework installed in your computer.
+
+Clone the Github repository on your computer, open the project folder in your terminal and run the command `ruby employees_payroll.rb`.
 
--- employees_payroll.rb
+++ employees_payroll.rb
@@ -2,14 +2,10 @@ require 'rails/all'
 require 'csv'
 require "awesome_print"
 
-#leemos todos los datos del csv
 csv_hcm = CSV.read("hcm.csv", headers: true)
-#ap csv_hcm.map{|l| l.to_h}
 
-#creamos el hash nuevo
 employees_data = Hash.new
 
-#de cada row seleccionamos el ID como identificador y rellenamos el hash nuevo
 csv_hcm.each do |employee|
     employees_data[ employee["ID"] ] = employee.to_h
 end
@@ -22,56 +18,47 @@ csv_payroll.each do |payroll|
     payrolls_data[payroll["worker_id"]] = payroll.to_h
 end
 
-#ap employees_data = payrolls_data.merge(employees_data)
+payrolls_data = employees_data.merge(payrolls_data)
 
- payrolls_data = employees_data.merge(payrolls_data)
-
-
-#preparamos las dos posibles uniones, las que dan error y las que estan ok
 merge = {errors: [], ok: []}
 
-# creamos metodo de validación
 def validate(e)
 
     if e['ID'].blank?
-        e[:errors] = "Falta ID"
-   end
-
-   if e['DNI'].blank?
-        e[:errors] = "Falta DNI"
+        e[:errors] = "Missing ID"
     end
 
-   if e['TELEPHONE'].blank?
-        e[:errors] = "Falta teléfono"
-   end
+    if e['DNI'].blank?
+        e[:errors] = "Missing DNI"
+    end
+
+    if e['TELEPHONE'].blank?
+        e[:errors] = "Missing telephone"
+    end
+
+    if e['EMAIL'].blank?
+        e[:errors] = "Missing email"
+    end
 
     e
 end
 
-#recorremos hash nominas
 payrolls_data.each do |payroll|
 
-    #traemos el id del empleado desde el cvs de empleados y del csv de nominas por si alguna no tiene datos de empleado
     employee = payrolls_data[payroll[0]] && employees_data[payroll[0]]
 
-    #comprobamos que ese empleado existe en el fichero
     if employee
 
-    #unimos la info de los dos csv segun la id
         employee_payroll = payroll[1].to_h.merge(employee)
 
-    #añadimos errores
         employee_payroll = validate(employee_payroll)
         
-    #mergeamos
         merge[:ok] << employee_payroll
       
-
-    #nominas sin empleado asignado
     else
         employee_payroll = payroll[1].to_h
         
-        employee_payroll[:errors] = 'Nómina sin datos de empleado'
+        employee_payroll[:errors] = 'Payroll without employee data'
            
         merge[:errors] << employee_payroll
     end
@@ -81,7 +68,7 @@ end
 
 CSV.open("result_#{Date.today.strftime}.csv", 'w') do |csv|
 
-    csv << %w(id dni name surname telephone email user_principal_name area iban start_date end_date company_id company_name net_salary extrapayroll_1 extrapayroll_2 extrapayroll_3, errors )
+    csv << %w(id dni name surname telephone email user_principal_name area iban start_date end_date company_id company_name net_salary extrapayroll_1 extrapayroll_2 extrapayroll_3 errors )
 
     merge[:ok].each do |employee| 
         csv << employee.values_at('ID', 'DNI', 'NAME', 'SURNAME', 'TELEPHONE', 'EMAIL', 'USER_PRINCIPAL_NAME', 'AREA', 'IBAN', 'START_DATE', 'END_DATE', 'COMPANY_ID', 'COMPANY_NAME', 'net_salary', 'extrapayroll_1', 'extrapayroll_2', 'extrapayroll_3', :errors)
@@ -90,9 +77,9 @@ end
 
 CSV.open("errors_#{Date.today.strftime}.csv", 'w') do |csv|
 
-    csv << %w(id dni name surname telephone email user_principal_name area iban start_date end_date company_id company_name net_salary extrapayroll_1 extrapayroll_2 extrapayroll_3 errors)
+    csv << %w(id dni name surname telephone email user_principal_name area iban start_date end_date company_id company_name net_salary extrapayroll_1 extrapayroll_2 extrapayroll_3 worker_id errors)
 
     merge[:errors].each do |employee| 
-        csv << employee.values_at('ID', 'DNI', 'NAME', 'SURNAME', 'TELEPHONE', 'EMAIL', 'USER_PRINCIPAL_NAME', 'AREA', 'IBAN', 'START_DATE', 'END_DATE', 'COMPANY_ID', 'COMPANY_NAME', 'net_salary', 'extrapayroll_1', 'extrapayroll_2', 'extrapayroll_3', :errors)
+        csv << employee.values_at('ID', 'DNI', 'NAME', 'SURNAME', 'TELEPHONE', 'EMAIL', 'USER_PRINCIPAL_NAME', 'AREA', 'IBAN', 'START_DATE', 'END_DATE', 'COMPANY_ID', 'COMPANY_NAME', 'net_salary', 'extrapayroll_1', 'extrapayroll_2', 'extrapayroll_3', 'worker_id', :errors)
     end
 end
